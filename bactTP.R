8590670ea7710c20073e2900690eb45dccbb475e
leobrocard <leo.brocard@unine.ch>
2024-05-26 13:33:39 +0200

    Update bactTP.R

--- bactTP.R
+++ bactTP.R
@@ -1,10 +1,13 @@
-# Initialisation des vecteurs
+#le code et les données proviennent des exercices sur R du début du semestre,
+#j'ai juste ajouté des trucs pour rendre le graph un peu plus complexe
+
+# vecteurs
 t <- 10
 vt <- c(10)
 b <- 1000
 vb <- c(1000)
 
-# Boucle pour calculer les températures et les données de population des bactéries
+# Boucle
 for (i in 1:30) {
   t <- t + 1
   vt <- c(vt, t)
@@ -16,14 +19,26 @@ for (i in 1:30) {
   vb <- c(vb, b)
 }
 
-# Calcul des courbes supérieures et inférieures
-vb_upper <- vb * 1.1
-vb_lower <- vb * 0.9
+#tendances supérieurs et inférieurs
+vb_up <- vb * 1.1
+vb_down <- vb * 0.9
 
 
-plot(vt, vb, type = "o", col = "blue", xlab = "Temps", ylab = "Population de bactéries", main = "Population de bactéries en fonction du temps")
-lines(vt, vb_upper, type = "o", col = "red")
-lines(vt, vb_lower, type = "o", col = "green")
+plot(vt, vb, 
+     type = "o",
+     col = "blue",
+     xlab = "Temps",
+     ylab = "Population de bactéries",
+     main = "Population de bactéries en fonction du temps")
+lines(vt, vb_up,
+      type = "o",
+      col = "red")
+lines(vt,
+      vb_down,
+      type = "o", col = "green")
 
 # légende
-legend("topright", legend = c("Valeurs réelles", "10% supérieur", "10% inférieur"), col = c("blue", "red", "green"), lty = 1, pch = 1)
+legend("topright",
+       legend = c("Valeurs réelles", "10% supérieur", "10% inférieur"),
+       col = c("blue", "red", "green"),
+       lty = 1, pch = 1)
